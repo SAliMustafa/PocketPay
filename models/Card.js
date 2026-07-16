3ebacc1c2ae91d509a6264bca169eaa95c032716
Sayed Ali <salimustafa2005@gmail.com>
2026-07-16 09:23:49 +0300

    Add Mongoose schemas for User, Card, and Transaction models

---
+++ models/Card.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const cardSchema = new mongoose.Schema({
+    balance:{
+        type: Number,
+        require: true
+    },
+    cardNumber:{
+        type: Number,
+        min: 4,
+        max: 4,
+        unique: true,
+        require: true
+    },
+    cardType: {
+        type: String,
+        require: true
+    },
+    expiryDate: {
+        type: String,
+        require: true
+    },
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, {timestamps: true})
+
+const Card = mongoose.model('Card', cardSchema)
+module.exports = Card 
---
+++ models/Transaction.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const transactionSchema = new mongoose.Schema({
+    transactionType:{
+        type: String,
+        require: true
+    },
+    amount: {
+        type: Number,
+        reqiore: true,
+        min: 0.1
+    },
+    transactionId: {
+        type: Number,
+    },
+    counterparty: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+
+    }
+}, {timestamps: true})
